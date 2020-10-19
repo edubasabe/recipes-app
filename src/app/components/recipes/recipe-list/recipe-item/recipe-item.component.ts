9c9c5167bc2a691dc2a09ab5932598ba6134e865
David Basabe <davidbasabe@MacBook-de-Karla.local>
2020-10-19 20:58:44 -0300

    :construction: App recipe binding

--- src/app/components/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/components/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Recipe } from '../../recipe.model';
 
 @Component({
@@ -7,11 +7,7 @@ import { Recipe } from '../../recipe.model';
   styles: [
   ]
 })
-export class RecipeItemComponent implements OnInit {
+export class RecipeItemComponent {
   @Input() recipe: Recipe;
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  @Output() recipeClicked: EventEmitter<any>;
 }
